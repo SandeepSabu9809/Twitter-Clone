9b33ebe0f057cda6d63c3c3a4f46e1bac096b351
sandeepsabu <sandeepsabu321@gmail.com>
2024-02-28 18:15:11 +0530

    changes correctedll

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-    // Define exportPathMap to enable static HTML export
-    exportPathMap: function () {
-        return {
-            '/': { page: '/' },
-            // Add other routes as needed
-        };
-    },
-    // other configurations...
-    output: {
-        // generate files for the specified routes
-        pages: '/',
-        // other configurations...
-    },
+const nextConfig = {
+    
 };
 
-module.exports = nextConfig;
+export default nextConfig;
