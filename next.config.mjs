c0cee12f1e2b1c01a2db5a2d886ebb531e9ebdae
sandeepsabu <sandeepsabu321@gmail.com>
2024-02-29 14:54:51 +0530

    changes saved

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // Define exportPathMap to enable static HTML export
+
 };
 
- module.exports = nextConfig
+export default nextConfig;
