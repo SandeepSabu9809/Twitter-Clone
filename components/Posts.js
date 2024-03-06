f0180c62fb7e071e0672413c1ba2565a92031a83
sandeepsabu <sandeepsabu321@gmail.com>
2024-03-06 11:03:00 +0530

    added liked post feature

--- components/Posts.js
+++ components/Posts.js
@@ -5,26 +5,43 @@ import { FaRegHeart } from "react-icons/fa";
 import { FiShare2 } from "react-icons/fi";
 import { HiOutlineChartBar } from "react-icons/hi";
 import Moment from "react-moment";
-import { doc, setDoc } from "firebase/firestore";
+import { collection, deleteDoc, doc, onSnapshot, setDoc, snapshotEqual } from "firebase/firestore";
 import { db } from "../firebase";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import customLoader from "./CustomImageLoader";
-// import { useEffect , useState } from "react";
+import { useEffect , useState } from "react"; 
+import { FaHeart } from "react-icons/fa";
 
 export default function Posts({post}) {
  
   const {data : session} = useSession();
-  // const [likes,setLikes] = useState([]);
+   const [likes,setLikes] = useState([]);
+   const [hasLiked,setHasLiked] = useState(false);
 
-  // useEffect(() => {
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, "posts" , post.id , "likes"), (snapshot) => setLikes(snapshot.docs)
+    )
+  },[db])
 
-  // },[db])
+  useEffect(()=>{
+      setHasLiked(likes.findIndex((like)=>like.id === session?.user.id) !== -1);
+  },[likes])
 
   async function likePost(){
-    await setDoc(doc(db, "posts" , post.id ,"likes" , session.user.id ), {
-      username : session.user.username, 
-    })
+    if(session){
+      if(hasLiked){
+        await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.id));
+      }else{
+        await setDoc(doc(db, "posts" , post.id ,"likes" , session?.user.id ), {
+          username : session.user.username, 
+        })
+      }
+    } else {
+      signIn(); 
+    }
+    
   }
 
   return (
@@ -64,7 +81,20 @@ export default function Posts({post}) {
               {/* icons */}
               <IoChatbubbleEllipsesOutline className="w-9 h-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
               <FaRegTrashCan className="w-9 h-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
-              <FaRegHeart onClick={likePost} className="w-9 h-9 hoverEffect p-2 hover:text-red-600 hover:bg-sky-100" /> 
+             <div className="flex items-center">
+              {hasLiked ? (
+                <>
+                <FaHeart onClick={likePost} className="w-9 h-9 hoverEffect p-2 text-red-600 hover:bg-sky-100" /> 
+                </>
+              ):(
+                <>
+                <FaRegHeart onClick={likePost} className="w-9 h-9 hoverEffect p-2 hover:text-red-600 hover:bg-sky-100" /> 
+                </>
+              )}
+              {likes.length > 0 && <span className={`${hasLiked && "text-red-600"} text-sm select-none`}  >{likes.length}</span>}
+             </div>
+
+             
               <FiShare2 className="w-9 h-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
               <HiOutlineChartBar className="w-9 h-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
             </div>
--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css';
 import { getProviders ,signIn } from 'next-auth/react';
 import Image from 'next/image';
+import customLoader from '../../../components/CustomImageLoader';
 
 export default function signin({providers}) {
   return (
@@ -8,6 +9,8 @@ export default function signin({providers}) {
       <Image
       width={44}
       height={80}
+      loader={customLoader}
+      unoptimized="true"
       src="https://spy.family/wp-content/uploads/2022/12/ch12findtimelinepngtwimg1920.png" 
       alt="twitter image inside a phone" 
       className='hidden object-cover md:w-44 md:h-80 rotate-6 md:inline-flex' 
@@ -15,7 +18,7 @@ export default function signin({providers}) {
       <div className="">
         {Object.values(providers).map((provider)=>(
             <div key={provider.id} className="flex flex-col items-center">
-                <Image width={36} height={36}  className='w-36 object-cover' src={"https://static.vecteezy.com/system/resources/previews/023/986/731/original/twitter-logo-twitter-logo-transparent-twitter-icon-transparent-free-free-png.png"} alt='twitter-logo'></Image>
+                <Image width={36} height={36} loader={customLoader} unoptimized="true"  className='w-36 object-cover' src={"https://static.vecteezy.com/system/resources/previews/023/986/731/original/twitter-logo-twitter-logo-transparent-twitter-icon-transparent-free-free-png.png"} alt='twitter-logo'></Image>
                 <p className='text-blue-800 text-center text-sm my-10 italic '>This app is created for learning purposes</p>
                 <button onClick={()=>signIn(provider.id, {callbackUrl:"/"})} className='bg-red-400 rounded-lg p-3 text-white hover:bg-red-500 ' >Sign in with {provider.name}</button>
             </div>
