878d12bc73ca0341b98f59c066f209a18f624f5f
sandeepsabu <sandeepsabu321@gmail.com>
2024-03-10 13:51:17 +0530

    testing g

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -1,4 +1,4 @@
-// import "../../app/globals.css";
+import "../../app/globals.css";
 import { getProviders, signIn } from 'next-auth/react';
 import Image from 'next/image';
 import customLoader from '../../../components/CustomImageLoader';
@@ -32,24 +32,26 @@ export default function Signin({ providers }) {
         className='hidden object-cover md:w-44 md:h-80 rotate-6 md:inline-flex'
       />
       <div className="">
-        {Object.values(providers).map((provider) => (
-          <div key={provider.id} className="flex flex-col items-center">
-            <Image
-              width={36}
-              height={36}
-              loader={customLoader}
-              unoptimized={true}
-              className='w-36 object-cover'
-              src={"https://static.vecteezy.com/system/resources/previews/023/986/731/original/twitter-logo-twitter-logo-transparent-twitter-icon-transparent-free-free-png.png"}
-              alt='Twitter logo'
-            />
-            <p className='text-blue-800 text-center text-sm my-10 italic'>
-              This app is created for learning purposes
-            </p>
-            <button onClick={() => signIn(provider.id, { callbackUrl: "/" })} className='bg-red-400 rounded-lg p-3 text-white hover:bg-red-500'>
-              Sign in with {provider.name}
-            </button>
-          </div>
+        {providers && Object.values(providers).map((provider) => (
+          <>
+            <div key={provider.id} className="flex flex-col items-center">
+              <Image
+                width={36}
+                height={36}
+                loader={customLoader}
+                unoptimized={true}
+                className='w-36 object-cover'
+                src={"https://static.vecteezy.com/system/resources/previews/023/986/731/original/twitter-logo-twitter-logo-transparent-twitter-icon-transparent-free-free-png.png"}
+                alt='Twitter logo'
+              />
+              <p className='text-blue-800 text-center text-sm my-10 italic'>
+                This app is created for learning purposes
+              </p>
+              <button onClick={() => signIn(provider.id, { callbackUrl: "/" })} className='bg-red-400 rounded-lg p-3 text-white hover:bg-red-500'>
+                Sign in with {provider.name}
+              </button>
+            </div>
+          </>
         ))}
       </div>
     </div>
